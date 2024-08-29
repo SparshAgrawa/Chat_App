42ee9b96cc2669008e8d07a3b6aa36d70789e753
SparshAgrawa <118478171+SparshAgrawa@users.noreply.github.com>
2024-08-30 02:38:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,27 +7,26 @@ import App from "./components/App";
 import chatting from "./reducers";
 
 // Define the logger middleware
-const logger = ({ dispatch, getState }) => (next) => (action) => {
-  console.log("ACTION", action);
-  return next(action);
-};
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log("ACTION", action);
+    return next(action);
+  };
 
-// Create the Redux store with middleware
 const store = createStore(chatting, applyMiddleware(logger, thunk));
 console.log("state", store.getState());
 
-// Create the StoreContext for passing the store and user information
 export const StoreContext = createContext();
 
-// Define user information
 const user = {
   name: "Sparsh",
   mobile: 9301511759,
   profilePic: require("./assets/myPhoto.jpg"),
-  contactId: 0, // like user id
+  contactId: 0,
 };
 
-// Define the Provider component
 const Provider = ({ userAndStore, children }) => {
   return (
     <StoreContext.Provider value={userAndStore}>
@@ -36,13 +35,11 @@ const Provider = ({ userAndStore, children }) => {
   );
 };
 
-// Define the user and store context value
 const userAndStore = {
   user: user,
   store: store,
 };
 
-// Render the App component wrapped in Provider
 ReactDOM.render(
   <Provider userAndStore={userAndStore}>
     <App />
